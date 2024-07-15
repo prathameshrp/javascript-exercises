891528f90119383ae8fbc67a2ccad3627e31597b
prathamesh <prathamesh.math@gmail.com>
2024-07-15 22:13:23 +0530

    modified:   07_tempConversion/tempConversion.js 	modified:   07_tempConversion/tempConversion.spec.js     solution 07 problems

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+/*
+celsius = (x  − 32) × ⁠5/9 °C
+fahrenheit = (x × ⁠9/5 + 32) °F
+*/
+
+const convertToCelsius = function(tempInFahrenheit) {
+  let tempInCelcius = (tempInFahrenheit - 32)*(5/9);
+  return Math.round(tempInCelcius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  let tempInFahrenheit = tempInCelcius*(9/5) +32;
+  return Math.round(tempInFahrenheit*10)/10;
 };
 
 // Do not edit below this line
