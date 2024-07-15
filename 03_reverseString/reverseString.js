5c98aab26f8387e65761aaddbed6da7ec2821c16
prathamesh <prathamesh.math@gmail.com>
2024-07-15 19:43:29 +0530

    modified:   03_reverseString/reverseString.js     2nd solution to 03 problem using split     separator and for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,17 @@
 const reverseString = function(str) {
     // solution - 1, using seperate variable
-    let r_str = "";
-    for(let i = str.length-1; i>=0; --i)
-        r_str+=str[i]
-    return r_str;
+    // let r_str = "";
+    // for(let i = str.length-1; i>=0; --i)
+    //     r_str+=str[i]
+    // return r_str;
+    let reversesStr = str.split("");
+    for(let i =0, j = str.length-1; i<=j; ++i, --j)
+    {
+        let temp = reversesStr[i];
+        reversesStr[i]=reversesStr[j];
+        reversesStr[j]=temp;
+    }
+    return reversesStr.join('');
 };
 
 // Do not edit below this line
