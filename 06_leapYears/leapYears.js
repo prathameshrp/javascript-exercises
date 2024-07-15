c733b2cef201488cc02f2b668056788048b9de71
prathamesh <prathamesh.math@gmail.com>
2024-07-15 21:33:34 +0530

    modified:   06_leapYears/leapYears.js 	modified:   06_leapYears/leapYears.spec.js     solution for 06 problem

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+/*
+    - year divisible by 4 is leap year
+    - year divisible by 100 is not leap year
+    - but if it's divisible by 400 then its leap year
+ */
 
+const leapYears = function(year) {
+    if(year%400 === 0) return true;
+    else if(year%4 === 0 && year%100 !== 0) return true;
+
+    return false;
 };
 
 // Do not edit below this line
