03b2a358cacf3fb6f5f0f2f4d71cf3975266f208
prathamesh <prathamesh.math@gmail.com>
2024-07-15 19:29:34 +0530

    modified:   02_repeatString/repeatString.js 	modified:   02_repeatString/repeatString.spec.js     solution for 02 javascript exercise problem,     passed all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0) return 'ERROR'
+    r_str="";
+    for(let i = 0; i<num; ++i)
+        r_str+=str;
+    return r_str;
 };
 
 // Do not edit below this line
