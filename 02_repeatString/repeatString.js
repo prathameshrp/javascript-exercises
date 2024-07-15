3e1c5cb4d9a3070ff940528d9ff3922f4eb34473
prathamesh <prathamesh.math@gmail.com>
2024-07-15 19:38:02 +0530

    modified:   02_repeatString/repeatString.js 	modified:   03_reverseString/reverseString.js 	modified:   03_reverseString/reverseString.spec.js     1st solution for reverseString using new     variable     updated variable r_str in 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,6 @@
 const repeatString = function(str, num) {
     if(num < 0) return 'ERROR'
-    r_str="";
+    let r_str="";
     for(let i = 0; i<num; ++i)
         r_str+=str;
     return r_str;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // solution - 1, using seperate variable
+    let r_str = "";
+    for(let i = str.length-1; i>=0; --i)
+        r_str+=str[i]
+    return r_str;
 };
 
 // Do not edit below this line
