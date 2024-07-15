599383b346dce31bf0c97b340f2ef91aa6417f59
prathamesh <prathamesh.math@gmail.com>
2024-07-15 20:14:03 +0530

    modified:   04_removeFromArray/removeFromArray.js 	modified:   04_removeFromArray/removeFromArray.spec.js
        solution for 04 js exercise problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    // let c_arr = arr;
+    // for(let element of elements)
+    // {
+    // let c_arr = arr.filter(function (e) {
+    //     e===element;
+    // });
+
+    // }
+    // return c_arr;
+    return arr.filter(val => !elements.includes(val));
 };
 
 // Do not edit below this line
