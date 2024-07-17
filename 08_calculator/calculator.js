19a9500456824b7db62d2f5333c75a336c840ac5
prathamesh <prathamesh.math@gmail.com>
2024-07-17 18:34:01 +0530

    Edit 08 problem javascript
        provided solution for 08 javascript problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	 return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    return arr.reduce((total, item) => total+item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    return arr.reduce((product, element) => product*element);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+    return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  return [...Array(n).keys()]
+          .map((e) => e+1)
+          .reduce((prod, item) => prod*item, 1);
 };
 
 // Do not edit below this line
