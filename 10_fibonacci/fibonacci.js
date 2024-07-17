25ab7b0e899ba2061159be410e8013e8768e2178
prathamesh <prathamesh.math@gmail.com>
2024-07-17 19:00:59 +0530

    Edit 10_fibonacci.js
        Solution for 10th js exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if(n < 0) return "OOPS";
+    if(n === 0) return 0;
+    const fibArr = [1, 1];
+    
+    for(let i = 2; i< n; ++i)
+    {
+        fibArr.push(fibArr[i-1]+fibArr[i-2]);
+    }
+    return fibArr[n - 1];
 };
 
 // Do not edit below this line
