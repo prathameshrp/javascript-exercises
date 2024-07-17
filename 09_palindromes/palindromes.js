42ff8f76688e474ebf1284fb27c47693fae87e6f
prathamesh <prathamesh.math@gmail.com>
2024-07-17 18:54:09 +0530

    Edit 09 js file
        Solution for 09 js exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const filter_str = str
+                    .toLowerCase()
+                    .split('')
+                    .filter((e) => alphanumerical.includes(e))
+                    .join('');
+                    
 
+    return filter_str === filter_str.split('').reverse().join('');
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
