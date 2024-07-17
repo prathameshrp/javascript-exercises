15dc567de3759ecc83349036da00fab5c9e4b531
prathamesh <prathamesh.math@gmail.com>
2024-07-17 19:37:59 +0530

    Edit 12_findTheOldest js file
        Solution to 12th and last js exercise problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        if(!a.yearOfDeath) a.yearOfDeath = (new Date()).getFullYear();
+        if(!b.yearOfDeath) b.yearOfDeath = (new Date()).getFullYear();
 
+       const lastGuy = a.yearOfBirth - a.yearOfDeath;
+       const nextGuy = b.yearOfBirth - b.yearOfDeath;
+        
+       return lastGuy > nextGuy ? 1 : -1;
+
+    })
+
+   return oldest[0];
 };
 
 // Do not edit below this line
